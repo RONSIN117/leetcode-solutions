ea8488f21484213e732ec1f10a2de4fbd3f545a7
RONIT SINHA <ronitsinha188@gmail.com>
2026-08-31 21:59:21 +0530

    [2091] [Time Beats: 81.36%] [Memory Beats: 91.42%] - LeetPush

--- 2091-Removing-Minimum-and-Maximum-From-Array.py
+++ 2091-Removing-Minimum-and-Maximum-From-Array.py
@@ -3,19 +3,14 @@ class Solution:
         i = nums.index(min(nums))
         j = nums.index(max(nums))
         
-        # Ensure i is the smaller index for simpler math
         if i > j:
             i, j = j, i
             
         n = len(nums)
         
-        # Strategy 1: Remove both from the front
         front = j + 1
         
-        # Strategy 2: Remove both from the back
         back = n - i
-        
-        # Strategy 3: Remove one from the front (up to i) and one from the back (down to j)
         both_sides = (i + 1) + (n - j)
         
         return min(front, back, both_sides)
